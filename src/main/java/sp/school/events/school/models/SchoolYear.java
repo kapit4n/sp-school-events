9300c83137627a40f3e0b1e99416c76a30b1d2f8
Luis Arce <luis.arce22@gmail.com>
2019-03-28 21:29:09 -0400

    Adding more fields to school year resource

--- src/main/java/sp/school/events/school/models/SchoolYear.java
+++ src/main/java/sp/school/events/school/models/SchoolYear.java
@@ -1,20 +1,58 @@
 package sp.school.events.school.models;
 
+import java.util.Date;
+
 public class SchoolYear {
 
     private final long id;
-    private final int year;
+    private final String year;
+    private final Date startDate;
+    private final Date endDate;
+    private final Boolean isCurrent;
+    private final Boolean isAchived;
 
-    public SchoolYear(long id, int year) {
+    public SchoolYear(long id, String year, Date startDate, Date endDate, Boolean isCurrent, Boolean isAchived) {
         this.id = id;
         this.year = year;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isCurrent = isCurrent;
+        this.isAchived = isAchived;
+    }
+
+    /**
+     * @return the isAchived
+     */
+    public Boolean getIsAchived() {
+        return isAchived;
+    }
+
+    /**
+     * @return the isCurrent
+     */
+    public Boolean getIsCurrent() {
+        return isCurrent;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
     }
 
     public long getId() {
         return id;
     }
 
-    public int getyear() {
+    public String getyear() {
         return year;
     }
 }
