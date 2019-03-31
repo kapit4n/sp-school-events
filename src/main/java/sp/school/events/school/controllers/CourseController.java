cb9cde56bbd3bf3121c5ec3f4b2b1864634de56d
Luis Arce <luis.arce22@gmail.com>
2019-03-31 17:41:29 -0400

    Save a course item

--- src/main/java/sp/school/events/school/controllers/CourseController.java
+++ src/main/java/sp/school/events/school/controllers/CourseController.java
@@ -3,6 +3,8 @@ package sp.school.events.school.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,17 @@ import sp.school.events.school.repositories.CourseRepository;
 public class CourseController {
 
     @Autowired
-    private CourseRepository courseRepository;
+    private CourseRepository repo;
 
 
     @RequestMapping("/courses")
     public List<Course> all() {
-        return this.courseRepository.findAll();
+        return this.repo.findAll();
+    }
+
+
+    @PostMapping("courses")
+    public Course save(@RequestBody Course newCourse) {
+        return repo.save(newCourse);
     }
 }
--- src/main/java/sp/school/events/school/models/Course.java
+++ src/main/java/sp/school/events/school/models/Course.java
@@ -29,6 +29,11 @@ public class Course {
     this.achived = achived;
   }
 
+  public Course()
+  {
+    
+  }
+
   /**
    * @return the achived
    */
